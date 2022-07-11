e9983b2379de09940780dca46d69ebd6f60152cf
Wiz <phamnahtbao1991@gmail.com>
2022-07-11 23:35:36 +0700

    add structure for multiple modules

---
+++ projects/wizard/src/lib/wizard.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-wizard',
+  template: `
+    <p>
+      wizard works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class WizardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
